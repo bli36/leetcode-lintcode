355297ab75eb99145bc12f7b62fc81d573a7efb3
sarahli <sarah.li@flysfo.com>
2019-09-19 10:01:55 -0700

    new q

--- src/FindJudgeSolution.java
+++ src/FindJudgeSolution.java
@@ -30,12 +30,31 @@ class FindJudgeSolution {
         return ans;
 
     }
+
+    public int findJudgeBetter(int N, int[][] trust){
+        int[] count = new int[N+1];
+        int ans = -1;
+        for (int i = 0; i < trust.length; i++){
+            count[trust[i][0]]--;
+            count[trust[i][1]]++;
+        }
+
+        for (int j = 1; j <= N; j++){
+            if (count[j] == N-1){
+                ans = j;
+            }
+        }
+
+        return ans;
+
+
+    }
     public static void main(String[] args) {
         int N = 4;
         int[][] trust = new int[][] {{1,3},{1,4},{2,3},{2,4},{4,3}};
 
         FindJudgeSolution s = new FindJudgeSolution();
-        int result = s.findJudge(N, trust); // Display the string.
+        int result = s.findJudgeBetter(N, trust); // Display the string.
         System.out.print("result=");
         System.out.println(result);
     }
