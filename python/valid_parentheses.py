0d515c3161440331b92f6eda397c0c04047a9842
Sarah <sherrylby@yahoo.com>
2024-01-21 21:08:35 -0800

    adding practice

---
+++ python/valid_parentheses.py
@@ -0,0 +1,45 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+ 
+
+# Example 1:
+
+# Input: s = "()"
+# Output: true
+# Example 2:
+
+# Input: s = "()[]{}"
+# Output: true
+# Example 3:
+
+# Input: s = "(]"
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of parentheses only '()[]{}'.
+
+
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {'(':')', '[':']', '{':'}'}
+        stack = deque()
+        for c in s:
+            if c == '(' or c == '[' or c == '{':
+                stack.append(c)
+            else:
+                if len(stack) == 0:
+                    return False
+                else:
+                    prev = stack.pop()
+                    if mapping[prev] != c:
+                        return False
+        return len(stack) == 0
